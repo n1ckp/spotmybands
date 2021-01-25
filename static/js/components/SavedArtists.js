fc627914f808dda084e1a51c8e946594261b464d
Nick Phillips <nicktgphillips@gmail.com>
2021-01-25 23:29:51 +0000

    Fix message bug, fix margin bug

--- static/js/components/SavedArtists.js
+++ static/js/components/SavedArtists.js
@@ -38,7 +38,7 @@ const SavedArtists = props => {
     <div id={styles.container}>
       <h1>Your Artists</h1>
       <div className={styles.actions}>
-        {filteredArtists.length > 0 && <Text
+        {hasArtists > 0 && <Text
           placeholder='Search for Artist...'
           onChange={text => setFilterText(text)}
           icon={<SearchIcon />} />}
