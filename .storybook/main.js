c381f415e775225bafce38df022104ef18f2bebe
Nick Phillips <nicktgphillips@gmail.com>
2021-03-05 17:42:24 +0000

    Set first default story to show for Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,10 @@
 const webpackFinal = require('./webpack.storybook.config');
 
 module.exports = {
-  stories: ['../src/**/*.stories.@(js)'],
+  stories: [
+    '../src/client/js/components/shared/widgets/Button.stories.js', // First story to show
+    '../src/**/*.stories.@(js)'
+  ],
   addons: [
     '@storybook/addon-essentials',
     '@storybook/addon-a11y',
