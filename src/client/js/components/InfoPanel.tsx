b6500e210c90c9f69ac93b21b45b0a5364e38f3e
Nick Phillips <nicktgphillips@gmail.com>
2021-03-07 15:46:12 +0000

    Cleanup About section

--- src/client/js/components/InfoPanel.tsx
+++ src/client/js/components/InfoPanel.tsx
@@ -5,10 +5,15 @@ const styles = require('./InfoPanel.scss').default
 const InfoPanel = () => {
   return (
     <div id={styles.container}>
-      <p>Created by <a href='https://n1ck.dev/'>Nick Phillips</a></p>
-      <img className={styles.spotify} src={require('@images/spotify.png').default} />
-      <img className={styles.songkick} src={require('@images/songkick.png').default} />
-      <a href="/storybook">Link to Storybook for this App</a>
+      <h1>About</h1>
+      <p>This site is maintained as a side project by <a href='https://n1ck.dev/'>Nick Phillips</a>.</p>
+      <p>Uses APIs from these sources:</p>
+      <div>
+        <img className={styles.spotify} src={require('@images/spotify.png').default} />
+        <img className={styles.songkick} src={require('@images/songkick.png').default} />
+      </div>
+      <hr />
+      <a href="/storybook">Storybook component library</a>
     </div>
   )
 }
